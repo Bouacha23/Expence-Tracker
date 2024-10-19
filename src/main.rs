31bd3acfa559ff9c11dac41d2661a82ef2af1c33
najme <bouachanajme@gmail.com>
2024-10-19 12:34:55 +0100

    add same fn delete summary

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use clap::{arg, Parser, Subcommand};
-use serde::{Deserialize};
-use std::{fs, io};
+use serde::{Deserialize , Serialize};
+use serde_json::{error, to_string_pretty, to_writer } ; 
+use std::{fs, io::{self, Write}, result};
+use std::fs::OpenOptions  ; 
+use chrono::{prelude, NaiveDate} ;
+use serde_json::to_string;
 
 /// Simple program to greet a person
 #[derive(Parser)]
@@ -21,21 +25,34 @@ enum Comme {
     },
     Delete {
         #[arg(short, long)]
-        target: u32,
+        id : Option<u32>
     },
     List,
     Summary {
         #[arg(short, long)]
-        month: Option<u32>,
+        month: Option<String>,
     },
+
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize , Serialize)]
 struct Track {
     id: u32,
     date: String,
     description: String,
     price: f64,  // Changed to f64 for consistency with `Comme::Add`
+}
+impl  Track {
+    fn new(id :u32 , description : String  , price : f64) -> Self {
+        let local_time = prelude::Local::now() ; 
+        let current_date = local_time.date_naive() ; 
+        Track {
+            id ,
+            date : current_date.to_string() , 
+            description , 
+            price
+        }
+    }
 }
 
 fn main() {
@@ -43,12 +60,19 @@ fn main() {
 
     match &args.command {
         Comme::Add { description, price } => {
-            println!("one element added: {} - {}", description, price);
-            // Here, you could add logic to save this new element to the JSON file.
+              let resute =   add_to_list(description , price) ; 
+              println!("{:?}" , resute)
         }
-        Comme::Delete { target } => {
-            println!("one element deleted: {:?}", target);
-            // Here, you could add logic to remove an element by id from the JSON file.
+        Comme::Delete {  id  } => {
+           match id  {
+              Some(id) =>
+               {
+                let resute  = delete_by_id(id)  ;
+            } ,
+            None => {
+                delet_all();
+            }     
+           }
         }
         Comme::List => {
             println!("showing list:");
@@ -57,8 +81,12 @@ fn main() {
             }
         }
         Comme::Summary { month } => match month {
-            Some(number) => println!("call fn summary for month: {}", number),
-            None => println!("call fn summary for all months"),
+            Some(number) =>  {
+                summary_by_month( number) ; 
+            }
+            None =>  {
+                let result = summary() ; 
+            }
         },
     }
 }
@@ -66,10 +94,141 @@ fn main() {
 fn list() -> io::Result<()> {
     let file = fs::read_to_string("Data.json")?;
     let expenses: Vec<Track> = serde_json::from_str(&file)?;
-
+    if expenses.len() == 0 {
+        println!("NO tracke existe") ; 
+        return   Ok(()) ; 
+    }
     println!("id    date            description     price:");
     for expense in expenses {
         println!("{}     {}      {}           {}", expense.id , expense.date ,expense.description , expense.price);
     }
     Ok(())
 }
+
+
+
+fn add_to_list(description : &String , price : &f64) -> Result<bool , std::io::Error>{
+    if  price.round() <= 1.00 || description.len() < 4 {
+         return  Ok(false) 
+    }else {
+        let file = fs::read_to_string("Data.json")?;
+        let mut expenses: Vec<Track> = serde_json::from_str(&file)?;
+        let id = get_id(&expenses) ; 
+        let new_track = Track::new(id, description.to_string(), *price);
+        expenses.push(new_track);
+
+       
+        
+        let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+
+        .open("Data.json")?;
+
+    let json = to_string_pretty(&expenses)?;
+    file.write_all(json.as_bytes())?; 
+
+        
+        
+    }
+    Ok(true)
+}
+
+
+fn get_id (list :&[Track]) -> u32 {
+   let mut last_id : u32 =  0; 
+   if list.len() == 0  {
+    return  last_id ; 
+   }else {
+     for  element in list {
+        last_id = element.id ; 
+     };
+     return  last_id + 1 ;
+   }
+   last_id 
+}
+
+
+fn delet_all () -> io::Result<()> {
+    let mut file = OpenOptions::new()
+        .write(true)
+        .truncate(true)
+        .open("Data.json")?; 
+    let arr : Vec<Track> = vec![] ; 
+    let json = to_string_pretty(&arr)?;
+    file.write_all(json.as_bytes())?;  
+    
+    Ok(())
+}
+
+fn delete_by_id (id : &u32) -> Result<() , io::Error>  {
+    let file = fs::read_to_string("Data.json")?;
+    let  expenses: Vec<Track> = serde_json::from_str(&file)?;
+    let mut new_list : Vec<Track> = vec![] ; 
+
+
+    for  track in expenses  {
+        if &track.id !=  id {
+            new_list.push( track);
+        }
+    }
+
+    println!("One track deleter  width id  {}" , id) ; 
+    let mut file = OpenOptions::new()
+    .write(true)
+    .truncate(true)
+    .open("Data.json")?; 
+    let json  :String = to_string_pretty(&new_list)?; 
+    file.write_all(json.as_bytes() )? ; 
+    Ok(())
+}
+
+
+fn summary () -> io::Result<()> {
+    let file = fs::read_to_string("Data.json")?;
+    let  expenses: Vec<Track> = serde_json::from_str(&file)?;
+
+    calculate_the_sammary( expenses , false ) ; 
+
+    Ok(())
+}
+
+
+fn calculate_the_sammary (expenses : Vec<Track> , type_of_fn   : bool)  {
+    let mut summary : f64  = 0.0 ; 
+    if  expenses.len() == 0 {
+        println!("Can't get summary list is empty ")  ; 
+    }else {
+    for track in &expenses {
+        summary += track.price 
+    }
+    if type_of_fn {
+        println!("Total expenses of the month is  : ${}" , summary ) ; 
+    }else {
+
+        println!("Total expenses : ${}" , summary ) ; 
+    }
+    }
+}
+
+
+fn summary_by_month (month : &String) -> io::Result<()> { 
+
+    let file = fs::read_to_string("Data.json")?;
+    let  expenses: Vec<Track> = serde_json::from_str(&file)?;
+    let resute = summary_filter_by_mother(expenses, month) ; 
+    calculate_the_sammary(resute , true) ; 
+    Ok(())
+}
+
+fn summary_filter_by_mother(expenses : Vec<Track> , month : &String)  ->  Vec<Track> {
+    let mut  new_arr : Vec<Track> =  vec![] ; 
+    for track in expenses {
+        let container =  track.date.contains(month) ; 
+        if container  {
+            new_arr.push(track);
+        }
+    }
+    new_arr
+}
+
